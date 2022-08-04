34f360c9bf6a76e00fea7dcc0c91c1afc09f3706
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2022-08-04 16:44:46 -0700

    Add item functionality in inventory

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    public GameObject player;
     //inventory load
     public GameObject inventory;
     public Button inventEnter;
     public Button inventExit;
+    //inventory items
+    public GameObject lion;
+    public GameObject hydra;
     //settings load
     public GameObject settings;
     public Button setEnter;
@@ -24,6 +28,10 @@ public class UI : MonoBehaviour
         Button inventExitBtn = inventExit.GetComponent<Button>();
 		inventExitBtn.onClick.AddListener(InventExit);
 
+        //inventory items
+        lion.active = false;
+        hydra.active = false;
+
         //settings
         settings.active = false;
         
@@ -31,6 +39,21 @@ public class UI : MonoBehaviour
 		setEnterBtn.onClick.AddListener(SetEnter);
         Button setExitBtn = setExit.GetComponent<Button>();
 		setExitBtn.onClick.AddListener(SetExit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //inventory items
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player.GetComponent<Inventory>().HasLionDrop())
+        {
+            lion.active = true;
+        }
+        if (player.GetComponent<Inventory>().HasHydraDrop())
+        {
+            hydra.active = true;
+        }
     }
 
     //inventory
